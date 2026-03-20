5f313e211e4bc64997fd987829e96d2c474b7064
John Lei Ulit <john.ulit@oqulo.com>
2026-03-20 11:18:14 +0800

    Comment out notif for submit application

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -202,12 +202,12 @@ class ApplicantController extends Controller
                     $application->competencyAssessments()->create($c);
                 }
                 // Send notification after successful transaction
-                try {
-                    $application->user->notify(new ApplicationSubmittedNotification($application));
-                } catch (\Exception $e) {
-                    // Log email error but don't fail the application submission
-                    \Log::error('Failed to send application notification: ' . $e->getMessage());
-                }
+                // try {
+                //     $application->user->notify(new ApplicationSubmittedNotification($application));
+                // } catch (\Exception $e) {
+                //     // Log email error but don't fail the application submission
+                //     \Log::error('Failed to send application notification: ' . $e->getMessage());
+                // }
             });     
 
             return redirect()->route('applicant.dashboard')->with('success', 'Application submitted.');
